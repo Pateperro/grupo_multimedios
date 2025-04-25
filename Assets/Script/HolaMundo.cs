f9be42c73ac2c4787e0b791f0aefe85968196ead
Miguel Vargas <mvargas662@unab.edu.co>
2025-04-25 07:19:17 -0500

    ajam

---
+++ Assets/Script/HolaMundo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Hola Mundo");
+        }
+    }
+}
